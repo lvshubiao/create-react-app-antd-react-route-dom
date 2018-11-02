678fa8860bdbfe12d759bd5b2c596072732b6449
lvshubiao <lvshubiao@bonc.com.cn>
2018-11-02 11:32:15 +0800

    删除测试文件

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import array from './array';
-import analytics from './analytics';
 import reducers from '../reducers';
 //var configureStore = createStore(reducers);
-var configureStore = applyMiddleware(thunk, array, analytics, createLogger)(createStore)(reducers);
+var configureStore = applyMiddleware(thunk, array, createLogger)(createStore)(reducers);
 
 export default configureStore;
